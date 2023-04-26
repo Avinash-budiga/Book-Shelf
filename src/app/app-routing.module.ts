7e8fb35f9ed14df692ead65832d0e0edd54ee9c0
avinash-budiga <avinash.budiga@acsicorp.com>
2023-04-26 13:31:35 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksearchComponent } from './components/booksearch/booksearch.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { BookshelfComponent } from './components/bookshelf/bookshelf.component';
+import { AuthrouteGuard } from 'src/guards/authroute.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'login', pathMatch:'full'},
+  {path:'login',  component: LoginComponent},
+  {path:'signup',  component: SignupComponent},
+  {path:'booksearch',  component: BooksearchComponent, 
+  // canActivate: [AuthrouteGuard]
+},
+  {path:'bookshelf', component: BookshelfComponent,
+  //  canActivate: [AuthrouteGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
