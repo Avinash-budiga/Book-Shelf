2ba15f1bef24023c4f982fcba846315711fa7241
avinash-budiga <avinash.budiga@acsicorp.com>
2023-04-27 16:34:44 +0530

    Revert "first commit"
    This reverts commit 7e8fb35f9ed14df692ead65832d0e0edd54ee9c0.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BooksearchComponent } from './components/booksearch/booksearch.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { BookshelfComponent } from './components/bookshelf/bookshelf.component';
-import { AuthrouteGuard } from 'src/guards/authroute.guard';
 
-const routes: Routes = [
-  {path:'', redirectTo: 'login', pathMatch:'full'},
-  {path:'login',  component: LoginComponent},
-  {path:'signup',  component: SignupComponent},
-  {path:'booksearch',  component: BooksearchComponent, 
-  // canActivate: [AuthrouteGuard]
-},
-  {path:'bookshelf', component: BookshelfComponent,
-  //  canActivate: [AuthrouteGuard]
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
